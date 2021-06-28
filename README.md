d764a9f7da71dc342b4eccc6e25f3e647ffb31af
August <1018555611@qq.com>
2021-06-28 17:35:17 +0800

    配置地址服务-初始化

---
+++ README.md
@@ -0,0 +1,3 @@
+### factory_api_manager
+### 地址管理器后台
+-- 用于app后获取最新的服务器地址
---
+++ src/config.js
@@ -0,0 +1,9 @@
+var config = {
+    database: 'config_manager', // 使用哪个数据库
+    username: 'root', // 用户名
+    password: 'Xiebao0301@', // 口令
+    host: 'ixiaomu.cn', // 主机名
+    port: 3306 // 端口号，MySQL默认3306
+};
+
+module.exports = config;
